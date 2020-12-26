cc9c1ec9a767fab97bb00caeaedcc94ae14f1ee6
vidyasagarkolusu <59787268+vidyasagarkolusu@users.noreply.github.com>
2020-12-26 19:27:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web-1" {
     #ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
+    key_name = "kubernetes"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
